19c28c4b7ba5c11158189ed9ddcfa736cdee712f
Alzeta Cahya <alzetacahya@gmail.com>
2024-09-20 10:07:03 +0700

    Styling Form Register and Login

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,6 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Login from './pages/Login';
 import Register from './pages/Register';
-import Home from './components/Home';
-
 
 function App() {
   return (
@@ -11,7 +9,6 @@ function App() {
       <Routes>
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
-        <Route path="/home" element={<Home />} />
       </Routes>
     </Router>
   );
--- src/components/AuthForm.jsx
+++ src/components/AuthForm.jsx
@@ -7,21 +7,21 @@ const AuthForm = ({ isLogin, onSubmit }) => {
   const [kelas, setKelas] = useState('');
   const [password, setPassword] = useState('');
   const [showPassword, setShowPassword] = useState(false);
-  
-  // Only for register form
   const [username, setUsername] = useState('');
-  const [phone, setPhone] = useState('');
 
   const navigate = useNavigate();
 
   const handleSubmit = (e) => {
     e.preventDefault();
+    
     if (isLogin) {
-      onSubmit(phone, password);
+      onSubmit(username, password);
     } else {
-      onSubmit(username, phone, kelas, password);  // Include additional fields for register
+      onSubmit(username, kelas, password);  
     }
-  };
+  
+    navigate('/welcome');
+  };  
 
   const togglePasswordVisibility = () => {
     setShowPassword(!showPassword);
@@ -30,44 +30,33 @@ const AuthForm = ({ isLogin, onSubmit }) => {
   return (
     <div className={styles.container}>
       <div className={styles.formBox}>
+        <img className={styles.image} src='/public/unnamed.png' alt=''></img>
         <h2 className={styles.heading}>{isLogin ? 'Login' : 'Register'}</h2>
         <form onSubmit={handleSubmit}>
           
-          {!isLogin && (
-            <>
-              <div className={styles.inputGroup}>
-                <label>Nama Lengkap</label>
-                <input 
-                  type="text" 
-                  value={username} 
-                  onChange={(e) => setUsername(e.target.value)} 
-                  required={!isLogin} 
-                />
-                <i><FaUser /></i>
-              </div>
-              <div className={styles.inputGroup}>
-                <label>Kelas</label>
-                <input 
-                  type="text" 
-                  value={kelas} 
-                  onChange={(e) => setKelas(e.target.value)} 
-                  required={!isLogin} 
-                />
-                <i><FaRegBuilding /></i>
-              </div>
-            </>
-          )}
-
           <div className={styles.inputGroup}>
-            <label>No Telepon</label>
+            <label>Nama Lengkap</label>
             <input 
-              type="tel" 
-              value={phone} 
-              onChange={(e) => setPhone(e.target.value)} 
-              required 
+              type="text" 
+              value={username} 
+              onChange={(e) => setUsername(e.target.value)} 
+              required
             />
-            <i><FaPhone /></i>
+            <i><FaUser /></i>
           </div>
+
+          {!isLogin && (
+            <div className={styles.inputGroup}>
+              <label>Kelas</label>
+              <input 
+                type="text" 
+                value={kelas} 
+                onChange={(e) => setKelas(e.target.value)} 
+                required 
+              />
+              <i><FaRegBuilding /></i>
+            </div>
+          )}
 
           <div className={styles.inputGroup}>
             <label>Password</label>
