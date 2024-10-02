e95f228ff51cecf4212a6d6d3c290e7b80048550
JustinReez <justinreez03@gmail.com>
2024-10-02 09:41:46 +0700

    adding port

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,16 +1,36 @@
-import React from 'react';
 import { useNavigate } from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
+import { LoginUser, reset } from '../features/AuthSlice';
 import AuthForm from '../components/AuthForm';
 
 const Login = () => {
   const navigate = useNavigate();
+  const dispatch = useDispatch();
+  const { user, isError, isSuccess, isLoading, message } = useSelector(
+    (state) => state.auth
+  );
+
+  useEffect(() => {
+    if (user || isSuccess) {
+      navigate('/dashboard');
+    }
+    dispatch(reset());
+  }, [user, isSuccess, dispatch, navigate]);
 
   const handleLogin = (email, password) => {
-    console.log('Login:', { email, password });
-    navigate('/dashboard');
+    dispatch(LoginUser({ email, password }));
   };
 
-  return <AuthForm isLogin={true} onSubmit={handleLogin} />;
+  return (
+    <div>
+      {isError && <p className="error">{message}</p>}
+      {isLoading ? (
+        <p>Loading...</p>
+      ) : (
+        <AuthForm isLogin={true} onSubmit={handleLogin} />
+      )}
+    </div>
+  );
 };
 
 export default Login;
